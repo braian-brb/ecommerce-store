f73eb351a05c3eabe8297bcaa84dd3481efe8b12
Braian Bilicic <braian.bilicic@gmail.com>
2022-08-24 12:33:37 -0300

    add @Param

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -18,5 +18,15 @@ export class AppController {
   @Get('/ruta/')
   hello() {
     return 'Con /sas/';
+  }
+
+  @Get('products/:id')
+  getProduct(@Param('id') id: string) {
+    return `product ${id}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(@Param('id') id: string, @Param('productId') productId: string) {
+    return `category ${id} and product ${productId}`;
   }
 }
