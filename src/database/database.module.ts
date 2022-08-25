8bc991d1085c756b5b01c3d5816e1201901552ff
Braian Bilicic <braian.bilicic@gmail.com>
2022-08-25 19:15:34 -0300

    Instalacion y configuracion mongoose

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module, Global } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { MongoClient } from 'mongodb';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import config from '../config';
 
@@ -9,6 +10,26 @@ const API_KEY_PROD = 'PROD123541323SA';
 
 @Global()
 @Module({
+  imports: [
+    // MongooseModule.forRoot('mongodb://localhost:27017', {
+    //   user: 'root',
+    //   pass: 'root',
+    //   dbName: 'ecommerce-store',
+    // }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigType<typeof config>) => {
+        const { connection, user, password, host, port, dbName } =
+          configService.mongo;
+        return {
+          uri: `${connection}://${host}:${port}`,
+          user,
+          pass: password,
+          dbName,
+        };
+      },
+      inject: [config.KEY],
+    }),
+  ],
   providers: [
     {
       provide: 'API_KEY',
@@ -28,6 +49,6 @@ const API_KEY_PROD = 'PROD123541323SA';
       inject: [config.KEY],
     },
   ],
-  exports: ['API_KEY', 'MONGO'],
+  exports: ['API_KEY', 'MONGO', MongooseModule],
 })
 export class DatabaseModule {}
