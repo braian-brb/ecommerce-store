33eabad8f03241f595bd48a2b946b1de22923634
Braian Bilicic <braian.bilicic@gmail.com>
2022-08-25 17:30:28 -0300

    docker-compose.yml

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-//TODO: Cambiar nombrea plurales
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
@@ -18,6 +18,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
