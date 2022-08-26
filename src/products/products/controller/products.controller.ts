13586d8b64838986ba551de0deebc74bacad2397
Braian Bilicic <braian.bilicic@gmail.com>
2022-08-26 09:36:27 -0300

    Control de roles en NestJS

--- src/products/products/controller/products.controller.ts
+++ src/products/products/controller/products.controller.ts
@@ -11,8 +11,12 @@ import {
   // ParseIntPipe,
 } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
-import { AuthGuard } from '@nestjs/passport';
 
+import { JwtAuthGuard } from '../../../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '../../../auth/guards/roles.guard';
+import { Public } from '../../../auth/decorators/public.decorator';
+import { Roles } from '../../../auth/decorators/roles.decorator';
+import { Role } from '../../../auth/models/roles.model';
 import { ProductService } from '../service/products.service';
 
 import { MongoIdPipe } from '../../../common/mongo-id.pipe';
@@ -22,24 +26,27 @@ import {
   FilterProductsDto,
 } from '../dto/products.dto';
 
-@UseGuards(AuthGuard('jwt'))
+@UseGuards(JwtAuthGuard, RolesGuard)
 @ApiTags('Products')
 @Controller('products')
 export class ProductController {
   constructor(private productsService: ProductService) {}
 
   @Get()
+  @Public()
   @ApiOperation({ summary: 'List of products' })
   getAll(@Query() params: FilterProductsDto) {
     return this.productsService.findAll(params);
   }
 
+  @Public()
   @Get('/:id')
   getOne(@Param('id', MongoIdPipe) id: string) {
     return this.productsService.findOne(id);
   }
 
   @Post()
+  @Roles(Role.ADMIN)
   create(@Body() payload: CreateProductDto) {
     return this.productsService.create(payload);
   }
