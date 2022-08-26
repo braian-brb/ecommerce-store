9cfcad0a419e351ce5e66c68bfb493b1e8c12a85
Braian Bilicic <braian.bilicic@gmail.com>
2022-08-25 23:28:00 -0300

    Usando un decorador

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards, SetMetadata } from '@nestjs/common';
 import { AppService } from './app.service';
 
 import { ApiKeyGuard } from './auth/guards/api-key.guard';
+import { Public } from './auth/decorators/public.decorator';
 
+@UseGuards(ApiKeyGuard)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Public()
   @Get()
   getHello() {
     return this.appService.getHello();
@@ -17,7 +20,7 @@ export class AppController {
     return this.appService.getTasks();
   }
 
-  @UseGuards(ApiKeyGuard)
+  @Public()
   @Get('nuevo')
   newEndPoint() {
     return 'Yo soy nuevo';
