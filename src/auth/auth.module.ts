a5525c5f89d8d682c3da5636c7dd6dd825ae654b
Braian Bilicic <braian.bilicic@gmail.com>
2022-08-26 00:21:59 -0300

    Ruta de login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,8 +4,10 @@ import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './strategies/local.strategy';
 import { AuthService } from './services/auth.service';
 import { UserModule } from '../users/users/module/users.module';
+import { AuthController } from './controllers/auth.controller';
 @Module({
   imports: [UserModule, PassportModule],
   providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
