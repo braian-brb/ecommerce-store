671be4de3aa93d4aab5f7e4d943ebdb741b237c4
Braian Bilicic <braian.bilicic@gmail.com>
2022-08-31 21:33:40 -0300

    add insomnia json to import

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -4,6 +4,7 @@ import { Request } from 'express';
 import { AuthService } from '../services/auth.service';
 import { User } from '../../users/users/entity/users.entity';
 import { ApiTags } from '@nestjs/swagger';
+// import { error } from 'console';
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
@@ -13,5 +14,12 @@ export class AuthController {
   login(@Req() req: Request) {
     const user = req.user as User;
     return this.authService.generateJWT(user);
+  }
+
+  //logout
+  @UseGuards(AuthGuard('jwt'))
+  @Post('logout')
+  logout(@Req() req: Request) {
+    return req.logout((error) => console.log(error));
   }
 }
